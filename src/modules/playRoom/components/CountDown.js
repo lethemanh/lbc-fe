620c436df05873dd02384866b9ec29096c804a11
nddmanh <manhnd.ilec@gmail.com>
2021-07-22 17:45:50 +0700

    Code UI Countdown clock.

---
+++ src/modules/playRoom/components/CountDown.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CountDown = () => {
+  return (
+    <div className="countdown">
+      <p id="time-countdown">
+        Thời gian còn lại: 
+        <strong> 59 </strong>
+      </p>
+    </div>
+  )
+}
+
+export default CountDown;
--- src/modules/playRoom/pages/PlayRoom.js
+++ src/modules/playRoom/pages/PlayRoom.js
@@ -3,12 +3,13 @@ import { Row, Col} from 'antd';
 import PickList from '../components/PickList';
 import './PlayRoom.css';
 import ChatBox from '../components/ChatBox';
+import CountDown from '../components/CountDown';
 
 function PlayRoom() {
   return (
     <Row justify="center" gutter={[32, 32]} >
       <Col xs={24} sm={24} md={24} xl={6} > 
-      
+        <CountDown />
       </Col>
       <Col xs={24} sm={24} md={24} xl={12} > 
         <PickList />

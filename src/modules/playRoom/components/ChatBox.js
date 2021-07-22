3de7080834640d8cc8cbbbaf372c6c47e3bc4fa4
nddmanh <manhnd.ilec@gmail.com>
2021-07-22 17:38:50 +0700

    Rewrite Chat UI to JSX.

---
+++ src/modules/playRoom/components/ChatBox.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const ChatBox = () => {
+  return (
+    <div className="noti">
+      <div className="chat-box">
+        
+        <div className="client">
+          <div className="client-info">
+            <h2>THÔNG BÁO</h2>
+          </div>
+        </div>
+        
+        <div className="scroll">
+          <div className="chats">
+
+            <div className="client-chat">
+              <strong>nddmanh</strong> vừa đặt cược <strong>100$</strong> vào
+              <strong>Cua</strong>
+            </div>
+            <div className="my-chat">
+              <strong>Bạn</strong> vừa đặt cược <strong>200$</strong> vào
+              <strong>Bầu</strong>
+            </div>
+            <div className="client-chat">
+              <strong>vhanh</strong> vừa đặt cược <strong>400$</strong> vào
+              <strong>Nai</strong>
+            </div>
+            <div className="my-chat">
+              <strong>Bạn</strong> vừa đặt cược <strong>100$</strong> vào
+              <strong>Gà</strong>
+            </div>
+            
+          </div>
+        </div>
+        
+        <form className="chat-input">
+          <input type="text" placeholder="Enter Message" />
+          <button type="submit" className="send-btn">
+            <img src="./send.png" alt="send-btn" />
+          </button>
+        </form>
+        
+      </div>
+    </div>
+  )
+}
+
+export default ChatBox;

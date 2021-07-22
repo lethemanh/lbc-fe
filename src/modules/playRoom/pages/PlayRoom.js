22dd2b1cfdebb69b74d4c15592f4369b97001adf
nddmanh <manhnd.ilec@gmail.com>
2021-07-22 17:32:56 +0700

    Rewrite UI pick and bet to JSX.

--- src/modules/playRoom/pages/PlayRoom.js
+++ src/modules/playRoom/pages/PlayRoom.js
@@ -2,22 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { Row, Col} from 'antd';
 import PickList from '../components/PickList';
 import './PlayRoom.css';
-import ChatBox from '../components/ChatBox';
-import ResultRoll from '../components/ResultRoll';
-import CountDown from '../components/CountDown';
 
 function PlayRoom() {
   return (
     <Row justify="center" gutter={[32, 32]} >
       <Col xs={24} sm={24} md={24} xl={6} > 
-        <CountDown />
-        <ResultRoll />
+        
       </Col>
       <Col xs={24} sm={24} md={24} xl={12} > 
         <PickList />
       </Col>
-      <Col xs={24} sm={24} md={24} xl={6} > 
-        <ChatBox />
+      <Col xs={24} sm={24} md={24} xl={6} >
+
       </Col>
     </Row>
   );

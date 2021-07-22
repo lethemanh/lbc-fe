0d8adc9d917017f411e3407e68a51dd696699bcb
nddmanh <manhnd.ilec@gmail.com>
2021-07-22 17:51:01 +0700

    Rewrite UI result to JSX.

--- src/modules/playRoom/pages/PlayRoom.js
+++ src/modules/playRoom/pages/PlayRoom.js
@@ -3,6 +3,7 @@ import { Row, Col} from 'antd';
 import PickList from '../components/PickList';
 import './PlayRoom.css';
 import ChatBox from '../components/ChatBox';
+import ResultRoll from '../components/ResultRoll';
 import CountDown from '../components/CountDown';
 
 function PlayRoom() {
@@ -10,6 +11,7 @@ function PlayRoom() {
     <Row justify="center" gutter={[32, 32]} >
       <Col xs={24} sm={24} md={24} xl={6} > 
         <CountDown />
+        <ResultRoll />
       </Col>
       <Col xs={24} sm={24} md={24} xl={12} > 
         <PickList />

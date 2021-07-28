dfbc358bd304494ce755f50c519764b3fce755f1
nddmanh <manhnd.ilec@gmail.com>
2021-07-28 16:46:06 +0700

    UI for countdown.

--- src/modules/playRoom/pages/PlayRoom.js
+++ src/modules/playRoom/pages/PlayRoom.js
@@ -1,8 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import { Row, Col} from 'antd';
+import CountDown from '../components/CountDown';
+import './PlayRoom.scss';
 
 function PlayRoom(props) {
   return (
-    <div></div>
+    <Row justify="center" >
+      <Col xs={24} xl={6} > 
+        <CountDown />
+      </Col>
+    </Row>
   );
 }
 

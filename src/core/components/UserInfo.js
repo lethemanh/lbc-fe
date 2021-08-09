f1cd69e3d1d9757ea0624a184a32ec97302df64a
nddmanh <manhnd.ilec@gmail.com>
2021-08-09 21:12:12 +0700

    Implement UserInfo Component in Header.

---
+++ src/core/components/UserInfo.js
@@ -0,0 +1,8 @@
+export default function UserInfo() {
+  return (
+    <div className="info-user">
+      <span className="info-user-name">Xin chào: <strong> Manh</strong> </span>
+      <span className="info-user-balance">Bạn đang có: <strong>1000 $</strong> </span>
+    </div>
+  );
+}

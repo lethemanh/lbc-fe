50d42276621299cec58c5c39e8aeb76f42918d64
le the manh <themanhle1234@gmail.com>
2021-08-10 21:01:33 +0700

    Fix router middleware

--- src/core/router/middlewares/auth.guard.js
+++ src/core/router/middlewares/auth.guard.js
@@ -3,7 +3,7 @@ import { isLoggedIn } from '../../helper/auth';
 const requireLogin = (to, from, next) => {
   if (to.meta.requireAuth && !isLoggedIn()) {
     next.redirect('/login');
-  } else if ((to.path === '/login' || to.path === 'register') && isLoggedIn()) {
+  } else if ((to.location.pathname === '/login' || to.location.pathname === '/register') && isLoggedIn()) {
     next.redirect('/');
   }
   next();
